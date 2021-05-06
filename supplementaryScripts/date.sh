c17b501b703025244ce7a78e2d38db5784874c95
MalcolmorianVII <bdkutambe@gmail.com>
2021-05-06 07:51:32 +0000

    Code optimization

--- supplementaryScripts/date.sh
+++ supplementaryScripts/date.sh
@@ -1,5 +1,24 @@
 #! /usr/bin/bash
+set -e
 
-cd /home/ubuntu/data/belson/isangi_nanopore/qc/results
-mkdir `date +"%Y.%m.%d"`
-ls
+D=`date +"%Y.%m.%d"`
+A=/home/ubuntu/data/belson/isangi_nanopore/qc/results/analysis
+P=/home/ubuntu/data/belson/isangi_nanopore/qc/results/polishing
+ 
+echo "Choose destination directory:a-Analysis & p-Polishing:"
+
+read REPLY
+
+if [ $REPLY == "a" ]
+then
+	mkdir -p $A/$D | ls $A
+
+elif  [ $REPLY == "p" ]
+then
+	mkdir -p $P/$D | ls $P
+
+else
+	echo "Invalid argument"
+fi
+
+exit 0
