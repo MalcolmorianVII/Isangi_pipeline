d43bdf80c75152ddeea1231943374bc76dc675b7
MalcolmorianVII <bdkutambe@gmail.com>
2021-06-23 06:50:00 +0000

    Updated to clair3

--- polishingScripts/clair.sh
+++ polishingScripts/clair.sh
@@ -3,27 +3,19 @@
 #NOTE:	$1 = {wildcards.sample}; $2 =  {wildcards.results}
 set -e 
 eval "$(conda shell.bash hook)"
-conda activate "clair-env"
+conda activate "clair3"
 
-model=/home/belson/data/clair/ont/model
+model=/home/belson/Clair3/models/ont_guppy5
 ref=$2/$1medaka/consensus.fasta
 bam=$2/$1.sorted.bam
-callVar=$2/$1CallVar
-flyeClair=$2/$1_flye_clair.vcf
-flyeClairgz=$flyeClair.gz
+
 normVCF=$2/$1_flye_clair.norm.vcf.gz
-clair.py callVarBamParallel --chkpnt_fn $model --ref_fn $ref --bam_fn $bam --threshold 0.2 --sampleName $1 --haploid_precision --includingAllContigs --output_prefix $callVar > $callVar.command.sh
-
-cat $callVar.command.sh | parallel -j2
-
-# prep vcf files for bcftools
-vcfcat $2/*.vcf > $flyeClair
-bgzip $flyeClair
-tabix -p vcf $flyeClairgz
-
+samtools faidx $ref	#Somehow giving an error of not finding fai file for consensus.fasta
+bash ~/Clair3/run_clair3.sh  --model_path=$model --ref_fn=$ref --bam_fn=$bam --platform="ont" --haploid_precise --output=$2 --threads=8 --include_all_ctgs
 #Normalizing indels  & indexing the normalized vcf files
-bcftools norm  -f $ref $flyeClairgz -m +any -Oz -o $normVCF
+conda activate "bcf"
+bcftools norm  -f $ref $2/merge_output.vcf.gz  -Oz -o $normVCF
 tabix -p vcf $normVCF
 
 #Altering the assembly
-bcftools consensus -f $ref $normVCF -H R > $2/$1_flye.clair.fasta
+bcftools consensus -f $ref $normVCF > $2/$1_flye.clair.fasta
