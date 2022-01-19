1d9cf46efc27e2da2b4a7c49db83815a5fe0f646
MalcolmorianVII <bdkutambe@gmail.com>
2022-01-19 12:41:41 +0000

    Code refactor

--- supplementaryScripts/workflowGenerator.sh
+++ supplementaryScripts/workflowGenerator.sh
@@ -4,7 +4,7 @@ set +eu
 eval "$(conda shell.bash hook)"
 conda activate snakemake
 # workflowgraph dir
-wf=/home/ubuntu/data/belson/isangi_nanopore/qc/scripts/graphs
+wf=/home/ubuntu/data/belson/bioinformatics/projects_2021/isangi_nanopore/scripts/graphs
 
 # $1=script
 
