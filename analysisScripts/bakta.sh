36475e7b36746f8be1edf70c84e3743986e8f9bd
Belson Malcolm Kutambe <67711995+MalcolmorianVII@users.noreply.github.com>
2025-02-09 13:45:17 +0200

    Delete analysisScripts/bakta.sh
    Removed the script because it was just there to activate special bakta conda env and running bakta within that env. Best practice is to have snakemake manage that automatically

--- analysisScripts/bakta.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-#NOTE:  $1 = {input}; $2 =  {output}
-set -e
-eval "$(conda shell.bash hook)"
-conda activate "bakta"
-
-bakta --db /home/ubuntu/data/belson/bakta/db/db $1 -o $2
-exit 0
